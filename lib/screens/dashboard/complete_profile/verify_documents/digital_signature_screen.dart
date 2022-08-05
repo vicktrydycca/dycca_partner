a4af4766952403f815f36739bb922a6e0d39e7bc
ritujaRK <rituja@wittyparrot.com>
2022-08-05 16:17:53 +0530

    5August2022

---
+++ lib/screens/dashboard/complete_profile/verify_documents/digital_signature_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DigitalSignatureScreen extends StatefulWidget {
+  const DigitalSignatureScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DigitalSignatureScreen> createState() => _DigitalSignatureScreenState();
+}
+
+class _DigitalSignatureScreenState extends State<DigitalSignatureScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
