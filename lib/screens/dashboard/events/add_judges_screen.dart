39732e0daaf353cc2ef3b57d514d5ae697351286
ritujaRK <rituja@wittyparrot.com>
2022-08-12 22:20:42 +0530

    12August2022

--- lib/screens/dashboard/events/add_judges_screen.dart
+++ lib/screens/dashboard/events/add_judges_screen.dart
@@ -17,11 +17,14 @@ class _AddJudgesScreenState extends State<AddJudgesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton:  FloatingActionButton(
+      floatingActionButton: FloatingActionButton(
         elevation: 0.0,
-        child:  const Icon(Icons.arrow_forward,color: whiteColour,),
-        backgroundColor:  primaryColor,
-        onPressed: (){
+        child: const Icon(
+          Icons.arrow_forward,
+          color: whiteColour,
+        ),
+        backgroundColor: primaryColor,
+        onPressed: () {
           Navigator.pushNamed(context, '/payOutPolicyRoutes');
         },
       ),
@@ -52,21 +55,23 @@ class _AddJudgesScreenState extends State<AddJudgesScreen> {
             height: 10,
           ),
           Padding(
-            padding: const EdgeInsets.symmetric( horizontal: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Visibility(
               visible: seen,
               child: Container(
                 decoration: BoxDecoration(
-                  border: Border.all(color: neutral4Color,width: 1),
-                  borderRadius: const BorderRadius.all(Radius.circular(4))
-                ),
+                    border: Border.all(color: neutral4Color, width: 1),
+                    borderRadius: const BorderRadius.all(Radius.circular(4))),
                 child: Column(
                   children: [
-                     Padding(
+                    Padding(
                       padding: const EdgeInsets.only(left: 70.0),
                       child: GestureDetector(
-                        onTap: (){},
-                          child: const Icon(Icons.cancel_rounded,size: 15,)),
+                          onTap: () {},
+                          child: const Icon(
+                            Icons.cancel_rounded,
+                            size: 15,
+                          )),
                     ),
                     const CircleAvatar(
                       radius: 20,
@@ -80,20 +85,20 @@ class _AddJudgesScreenState extends State<AddJudgesScreen> {
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(bottom: 8.0,left: 20,right: 20),
+                      padding: const EdgeInsets.only(
+                          bottom: 8.0, left: 20, right: 20),
                       child: Text(
                         "@JackMan123",
                         style: fontStyle(neutral6Color, FontWeight.w100, 10),
                       ),
                     ),
-
                   ],
                 ),
               ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 20.0,top: 15),
+            padding: const EdgeInsets.only(left: 20.0, top: 15),
             child: Text(
               "Recently Tagged",
               style: fontStyle(neutral6Color, FontWeight.w100, 19),
@@ -106,66 +111,69 @@ class _AddJudgesScreenState extends State<AddJudgesScreen> {
               physics: const ScrollPhysics(),
               shrinkWrap: true,
               itemBuilder: (context, index) {
-              return ListTile(
-                leading: const CircleAvatar(
-                  radius: 23,
-                  backgroundColor: Colors.blue,
-                ),
-                title: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "JackMan",
-                      style: fontStyle(neutral6Color, FontWeight.w100, 16),
-                    ),
-                    Text(
-                      "@JackMan123",
-                      style: fontStyle(neutral4Color, FontWeight.w100, 14),
-                    )
-                  ],
-                ),
-                trailing:  Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      setState((){
-                        seen = true;
-                      });
-                      // Navigator.pushNamed(context, '/addJudgesRoutes');
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: primaryColor.withOpacity(1),
-                        borderRadius:
-                        BorderRadius.circular(3.0),
+                return ListTile(
+                  leading: const CircleAvatar(
+                    radius: 23,
+                    backgroundColor: Colors.blue,
+                  ),
+                  title: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "JackMan",
+                        style: fontStyle(neutral6Color, FontWeight.w100, 16),
                       ),
-                      height: 30,
-                      width: 70,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Icon(Icons.add,size: 15,color: whiteColour,),
-                            Text(
-                              "  ADD",
-                              style: appFontStyle(
-                                color:
-                                Colors.white.withOpacity(0.9),
-                                fontSize: 12,
-                                fontWeight: FontWeight.w700,
+                      Text(
+                        "@JackMan123",
+                        style: fontStyle(neutral4Color, FontWeight.w100, 14),
+                      )
+                    ],
+                  ),
+                  trailing: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          seen = true;
+                        });
+                        // Navigator.pushNamed(context, '/addJudgesRoutes');
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: primaryColor.withOpacity(1),
+                          borderRadius: BorderRadius.circular(3.0),
+                        ),
+                        height: 30,
+                        width: 70,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const Icon(
+                                Icons.add,
+                                size: 15,
+                                color: whiteColour,
                               ),
-                            ),
-                          ],
+                              Text(
+                                "  ADD",
+                                style: appFontStyle(
+                                  color: Colors.white.withOpacity(0.9),
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              );
-            },),
+                );
+              },
+            ),
           )
         ],
       ),
